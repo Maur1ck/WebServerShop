2cfee5fd11d754623c957cb0e948b3eb8f93ab7b
Misha <work@maiol.ru>
2023-04-20 18:24:58 +0300

    Исправил ORM модель и обновилась Бд после запуска

--- data/products.py
+++ data/products.py
@@ -5,7 +5,7 @@ from .db_session import SqlAlchemyBase
 class Product(SqlAlchemyBase):
     __tablename__ = 'products'
 
-    id = sqlalchemy.Column(sqlalchemy.String,
+    id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
