52624d1a8f4ddc16905bc0dcc249c1921e4c933c
Misha <work@maiol.ru>
2023-04-24 17:08:09 +0300

    Написал для каждой html страницы маршрут и все отладил

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, jsonify
+from flask_restful import Resource, Api
+from flask_login import LoginManager, login_user, login_required, logout_user
 from data import db_session
 from data.products import Product
 from data.users import User
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
+from resources.resources import ProductResource, ProductListResource
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+api.add_resource(ProductListResource, '/api/products')
+api.add_resource(ProductResource, '/api/products/<int:product_id>')
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+
+    return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -22,14 +51,29 @@ def reqister():
                                    form=form,
                                    message="Такой пользователь уже есть")
         user = User(
-            name=form.username.data,
+            username=form.username.data,
             email=form.email.data,
         )
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/products')
+        return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/products', methods=['GET', 'POST'])
@@ -50,6 +94,21 @@ def products():
 
     # render the HTML template and pass in the products data
     return render_template('products.html', products=products)
+
+
+@app.route('/cart')
+def cart():
+    db_sess = db_session.create_session()
+
+    cart = session.get('cart', {})
+
+    product_ids = list(cart.keys())
+
+    products = db_sess.query(Product).filter(Product.id.in_(product_ids)).all()
+
+    total = sum([product.price * cart[str(product.id)] for product in products])
+
+    return render_template('cart.html', products=products, cart=cart, total=total)
 
 
 def main():
